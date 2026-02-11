a24399f5748640f08b8a3860cdbdb368196a3b56
Akbari <60307443@udst.edu.qa>
2026-02-11 19:29:17 +0300

    Fix word deletion bug (Fixes #1)

--- script.js
+++ script.js
@@ -112,6 +112,7 @@ function editWord(index) {
 
 function deleteWord(index) {
     if (confirm('Are you sure you want to delete this word?')) {
+        wordBank.splice(index, 1)
         saveWordBank();
         displayWordBank();
     }
